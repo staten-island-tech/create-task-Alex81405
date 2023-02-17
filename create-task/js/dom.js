a3208b206d1d57a596e2fdb97453445a4437fa2c
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2023-02-17 10:01:06 -0500

    AP Create Task

--- create-task/js/dom.js
+++ create-task/js/dom.js
@@ -1,5 +1,5 @@
 const DOMSelectors = {
-  factButton: document.getElementById("getfactButton"),
+  factButton: document.getElementById("getFactButton"),
   randomfact: document.getElementById("fact"),
   searchButton: document.getElementById("searchButton"),
   searchInput: document.getElementById("searchInput"),
